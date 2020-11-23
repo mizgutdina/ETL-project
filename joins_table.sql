c6ae1160348e6c6797f649bd7eff89ef9914331a
diazdez <69435783+diazdez@users.noreply.github.com>
2020-11-22 19:34:30 -0600

    drafted word doc of the project

--- joins_table.sql
+++ joins_table.sql
@@ -1,5 +1,5 @@
 -- Joins tables sample
--- SELECT income_data.zipCode, income_data.state, income_data.total_income, income_data.state, starbucks_data.zipCode, starbucks_data.state, starbucks_data.storeNumber, starbucks_data.storeName 
--- FROM income_data
--- JOIN starbucks_data
--- ON income_data.zipCode = income_data.zipCode;
+SELECT income_data.zipCode, income_data.state, income_data.total_income, starbucks_data.zipCode, starbucks_data.state, starbucks_data.storeNumber, starbucks_data.storeName 
+FROM income_data
+JOIN starbucks_data
+ON income_data.zipCode = income_data.zipCode;
