b79b5f706b59068f9864034435b602cfdf0f7697
diazdez <69435783+diazdez@users.noreply.github.com>
2020-11-22 18:19:43 -0600

    draft JOINS sql

---
+++ joins_table.sql
@@ -0,0 +1,5 @@
+-- Joins tables sample
+-- SELECT income_data.zipCode, income_data.state, income_data.total_income, income_data.state, starbucks_data.zipCode, starbucks_data.state, starbucks_data.storeNumber, starbucks_data.storeName 
+-- FROM income_data
+-- JOIN starbucks_data
+-- ON income_data.zipCode = income_data.zipCode;
